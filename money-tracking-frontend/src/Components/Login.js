61c514f867e382a57119746d981a9126d2175efa
emrekocadere <emrekocadere34@gmail.com>
2024-04-06 19:19:55 +0300

    Feat: Implement login form and transition animation

--- money-tracking-frontend/src/Components/Login.js
+++ money-tracking-frontend/src/Components/Login.js
@@ -10,15 +10,49 @@ import {
     Row,
     Select,
 } from 'antd';
-import { useState } from 'react';
+import "../App.css";
 import { Typography } from "antd";
+import { useTransition, animated } from '@react-spring/web'
+import React, { useState, useCallback, useRef, useEffect } from 'react'//bunlara ayrı ayrı bak
 const { Title } = Typography;
-
 const { Option } = Select;
 
-
 const Login = () => {
-    const [form] = Form.useForm();
+
+
+    const ref = useRef([]);
+    const [items, set] = useState([]);
+    const transitions = useTransition(items, {
+        from: {
+            opacity: 0,
+            height: 0,
+            innerHeight: 0,
+            transform: 'perspective(600px) rotateX(0deg)',
+            color: '#8fa5b6',
+        },
+        enter: [
+            { opacity: 1, height: 80, innerHeight: 80 },
+            { transform: 'perspective(600px) rotateX(180deg)', color: '#28d79f' },
+            { transform: 'perspective(600px) rotateX(0deg)' },
+        ],
+        leave: [{ color: '#c23369' }, { innerHeight: 0 }, { opacity: 0, height: 0 }],
+        update: { color: '#28b4d7' },
+    });
+
+    const reset = () => {
+        ref.current.forEach(clearTimeout);
+        ref.current = [];
+        set([]);
+        ref.current.push(setTimeout(() => set(['Apples', 'Oranges', 'Kiwis']), 2000));
+        ref.current.push(setTimeout(() => set(['Apples', 'Kiwis']), 5000));
+        ref.current.push(setTimeout(() => set(['Apples', 'Bananas', 'Kiwis']), 8000));
+    };
+    useEffect(() => {
+        reset()
+        return () => ref.current.forEach(clearTimeout)
+    }, [])
+
+    const [form] = Form.useForm();//buna bak ant designın default kodu mu
 
     const onFinish = async (values) => {
         console.log('Received values of form: ', values);
@@ -40,21 +74,15 @@ const Login = () => {
     }));
     return (
         <div style={{ background: "#f2f2f2 ", display: 'flex', height: '100vh', alignItems: "center" }}>
-            <div style={{
+            <div className="LoginRegisterCard">
 
-
-                marginLeft: "10vw",
-                backgroundColor: 'white',
-                borderRadius: '2vw',
-
-            }}>
                 <Title level={2} style={{
                     display: "flex", justifyContent: "center",
                     color: "white", background: "#278664",
                     margin: "0px", borderTopLeftRadius: '2vw',
-                    borderTopRightRadius: '2vw',height:"7vh",
-                    alignItems:"center",justifyContent:"center"
-                    
+                    borderTopRightRadius: '2vw', height: "7vh",
+                    alignItems: "center"
+
                 }}>
                     Login
                 </Title>
@@ -68,12 +96,11 @@ const Login = () => {
                         prefix: '+90',
                     }}
                     style={{
-                        padding:"10vh"
+                        padding: "10vh"
 
                     }}
                     scrollToFirstError
                 >
-
 
                     <Form.Item
                         name="username"
@@ -89,8 +116,6 @@ const Login = () => {
                     >
                         <Input />
                     </Form.Item>
-
-
 
                     <Form.Item
                         name="password"
@@ -117,8 +142,14 @@ const Login = () => {
 
 
                 </Form>
+
             </div>
 
+                {transitions(({ innerHeight, ...rest }, item) => (
+                    <animated.div  style={rest} onClick={reset}>
+                        <animated.div style={{ overflow: 'hidden', height: innerHeight }}>{item}</animated.div>
+                    </animated.div>
+                ))} 
 
         </div>
     );
