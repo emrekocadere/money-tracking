f948fdcb649c9b8a15df963750dfdfd59da44ed3
emrekocadere <emrekocadere34@gmail.com>
2024-04-19 00:06:36 +0300

    Feat: Add AddIncome component and define routes

--- money-tracking-frontend/src/App.js
+++ money-tracking-frontend/src/App.js
@@ -7,9 +7,26 @@ import NavigationBar from './Components/NavigationBar';
 import AddIncome from './Components/AddIncome';
 function App() {
   return (
-    <div className='LoginRegisterComponent'>
-    <AddIncome/>
-    </div>
+    <div className="App">
+
+    <BrowserRouter>
+
+      <Routes>
+      
+      <Route path="/" element={<Login />}/>
+      <Route path="register" element={<Register/>}/>
+      <Route path="dashboard" element={<Dashboard/>}/>
+      <Route path="Graphic" element={<Graphic/>}/>
+      <Route path="Profile" element={<Profile />} />
+      <Route path="income" element={<Incomes />} />
+      <Route path="expenses" element={<Expenses/>}/>
+      <Route path="*" element={<CustomNavigation />} />
+      
+      </Routes>
+
+    </BrowserRouter>
+
+  </div>
 
   );
 }
