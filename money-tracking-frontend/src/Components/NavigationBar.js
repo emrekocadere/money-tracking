411cf5185c2516307beb7d888c47c61f88bbdb9c
emrekocadere <emrekocadere34@gmail.com>
2024-04-19 00:16:47 +0300

    Refactor: Update NavigationBar to use React Router's Link component

--- money-tracking-frontend/src/Components/NavigationBar.js
+++ money-tracking-frontend/src/Components/NavigationBar.js
@@ -10,13 +10,13 @@ function getItem(label, key, icon, children, type) {
     };
 }
 const items = [
-    getItem("/Dashboard", "genel"),
-    getItem("income", "girdiler"),
-    getItem("expenses", "çıktılar"),
-    getItem("Last Month", "1ay"),
-    getItem("Profile", "profil"),
-    getItem("Settings", "ayarlar"),
-    getItem( "Exit","çıkış"),
+    getItem(<Link to="/Dashboard">Dashboard</Link>, "genel"),
+    getItem(<Link to="/income">Incomes</Link>, "girdiler"),
+    getItem(<Link to="/expenses">Expenses</Link>, "çıktılar"),
+    getItem(<Link to="/Profile">last 1 month</Link>, "1ay"),
+    getItem(<Link to="/Profile">Profil</Link>, "profil"),
+    getItem(<Link to="/Profile">Settings</Link>, "ayarlar"),
+    getItem(<Link to="/Profile">Exit</Link>, "çıkış"),
 ];
 const NavigationBar = () => {
     const onClick = (e) => {
