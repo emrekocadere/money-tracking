a43a3360d55ab3ffb42bb0b3a20d1c79cf1f5a71
emrekocadere <emrekocadere34@gmail.com>
2024-05-01 23:22:53 +0300

    Feat: Add MoneyTrackingDbContext

---
+++ MoneyTrackingBackend/MoneyTrackingDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MoneyTrackingBackend.Entities;
+
+namespace MoneyTrackingBackend
+{
+	public class MoneyTrackingDbContext:IdentityDbContext
+	{
+        public MoneyTrackingDbContext(DbContextOptions<MoneyTrackingDbContext> options) :base(options) { }
+        public DbSet<Income> Incomes { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
+
