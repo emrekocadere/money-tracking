a940453450bdd3cbc54fdfb58644c223660b72d1
emrekocadere <emrekocadere34@gmail.com>
2024-05-01 23:02:22 +0300

    Feat: Add entities

---
+++ MoneyTrackingBackend/Entities/Expense.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace MoneyTrackingBackend.Entities
+{
+    public class Expense
+    {
+
+        public int Id { get; set; }
+        public int Amount { get; set; }
+        public int NewBalance { get; set; }
+        public string Description { get; set; }
+        public string UserId { get; set; }
+
+    }
+}
+
---
+++ MoneyTrackingBackend/Entities/Role.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace MoneyTrackingBackend.Entities
+{
+	public class Role:IdentityRole
+	{
+
+	}
+}
+
