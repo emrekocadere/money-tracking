db98b5ee0edb095553a94a37b9aa2e07add03729
emrekocadere <emrekocadere34@gmail.com>
2024-05-01 23:50:11 +0300

    Feat: Update MoneyTrackingDbContext to use custom IdentityUser, Role, and primary key type

--- MoneyTrackingBackend/MoneyTrackingDbContext.cs
+++ MoneyTrackingBackend/MoneyTrackingDbContext.cs
@@ -5,11 +5,12 @@ using MoneyTrackingBackend.Entities;
 
 namespace MoneyTrackingBackend
 {
-	public class MoneyTrackingDbContext:IdentityDbContext
-	{
+	public class MoneyTrackingDbContext:IdentityDbContext<User, Role, int>
+    {
         public MoneyTrackingDbContext(DbContextOptions<MoneyTrackingDbContext> options) :base(options) { }
         public DbSet<Income> Incomes { get; set; }
         public DbSet<Expense> Expenses { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
 
