6447e2a5522761e2cfda3fd617e6e4be13695535
emrekocadere <emrekocadere34@gmail.com>
2024-06-27 00:43:46 +0300

    Feat: add Transaction entity class

---
+++ MoneyTrackingBackend/Entities/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace MoneyTrackingBackend.Entities
+{
+	public class Transaction
+	{
+        public int Id { get; set; }
+        public int Amount { get; set; }
+        public int NewBalance { get; set; }
+        public string Description { get; set; }
+        public User User { get; set; }
+        public int UserId { get; set; }
+    }
+}
+
