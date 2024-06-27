7e7219766ee5b51c3c899d9f04a9bd02b86d80ac
emrekocadere <emrekocadere34@gmail.com>
2024-06-27 20:54:07 +0300

    Feat: add SignInDTO and SignUpDTO classes for user authentication

---
+++ MoneyTrackingBackend/DTOs/SignInDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MoneyTrackingBackend.DTOs
+{
+	public class SignInDTO
+	{
+		public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
+
---
+++ MoneyTrackingBackend/DTOs/SignUpDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MoneyTrackingBackend.DTOs
+{
+	public class SignUpDTO
+	{
+		public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Balance { get; set; }
+    }
+}
+
