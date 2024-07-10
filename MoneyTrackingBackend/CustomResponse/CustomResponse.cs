f4ed1b2cf8e6f43402f91f246d038b1eac0a3005
emrekocadere <emrekocadere34@gmail.com>
2024-07-10 20:44:04 +0300

    Feat: add CustomResponse class

---
+++ MoneyTrackingBackend/CustomResponse/CustomResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MoneyTrackingBackend.CustomResponse
+{
+	public class CustomResponse
+	{
+
+		public CustomStatusCode StatusCode;
+		public string StatusDesc;
+		public List<string> Messages { get; set; }
+		public Dictionary<string, object> ExtraDatas { get; set; }
+	}
+}
+
---
+++ MoneyTrackingBackend/CustomResponse/CustomStatusCode.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace MoneyTrackingBackend.CustomResponse
+{
+	public enum CustomStatusCode
+	{
+
+	}
+}
+
