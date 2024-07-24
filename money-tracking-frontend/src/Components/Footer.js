150cbb101cd87abc66c2760eb2dda1a2096c227c
emrekocadere <emrekocadere34@gmail.com>
2024-07-24 11:09:45 +0300

    Feat: add MediumOutlined icon and update links

--- money-tracking-frontend/src/Components/Footer.js
+++ money-tracking-frontend/src/Components/Footer.js
@@ -3,7 +3,8 @@ import { Flex, Layout } from 'antd';
 import { Space, Typography } from 'antd';
 import {
     GithubOutlined,
-    LinkedinFilled
+    LinkedinFilled,
+    MediumOutlined
 } from '@ant-design/icons';
 const { Header, Footer, Sider, Content } = Layout;
 const { Text, Link } = Typography;
@@ -13,8 +14,9 @@ const CustomFooter = () => {
     return (
         <Footer>
             <div>Developed By Salih Emre Kocadere</div>
-            <Link href="https://google.com" target="_blank" style={{ fontSize: "3vh", marginRight: "1.5vw" }}><GithubOutlined /></Link>
-            <Link href="https://google.com" target="_blank" style={{ fontSize: "3vh" }}><LinkedinFilled /></Link>
+            <Link href="https://github.com/emrekocadere?tab=overview&from=2024-07-01&to=2024-07-24" target="_blank" style={{ fontSize: "3vh", marginRight: "1.5vw" }}><GithubOutlined /></Link>
+            <Link href="https://google.com" target="_blank" style={{ fontSize: "3vh", marginRight: "1.5vw" }}><LinkedinFilled /></Link>
+            <Link href="https://medium.com/@salihemrekocadere" target="_blank" style={{ fontSize: "3vh" }}><MediumOutlined /></Link>
         </Footer>
 
     );
